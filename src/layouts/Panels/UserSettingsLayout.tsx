1af3890c31e729e9904a678cc3ed96811493bdb0
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-10-23 12:38:33 +0800

    feat: add change password functionality (#52)

--- src/layouts/Panels/UserSettingsLayout.tsx
+++ src/layouts/Panels/UserSettingsLayout.tsx
@@ -3,10 +3,10 @@ import TopHeader from '../../components/TopHeader'
 import tw from 'twrnc'
 import { fonts } from '../../styles/global'
 import { MaterialIcon } from '../../utils/Icons'
-import { ScrollView, View, Text, TextInput, TouchableOpacity } from 'react-native'
-import { useUpdateAccount } from '../../lib/ReactQuery'
 import { Toast } from '../../utils/Toast'
+import { ScrollView, View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { useNavigate } from '../../utils/RootNavigation'
+import { useUpdateAccount, useChangePassword } from '../../lib/ReactQuery'
 
 interface TypedProps {
   user: any
@@ -16,6 +16,11 @@ interface TypedProps {
 const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
 
   const updateAccount = useUpdateAccount()
+  const changePassword = useChangePassword()
+
+  // loading state
+  const [isUpdateAccountLoading, setIsUpdateAccountLoading] = React.useState<boolean>(false)
+  const [isChangePasswordLoading, setIsChangePasswordLoading] = React.useState<boolean>(false)
 
   // form state
   const [name, setName] = React.useState<string>(user.name)
@@ -29,34 +34,93 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
   const [rePassword, setRePassword] = React.useState<string>('')
 
   // validation state
+  const [nameError, setNameError] = React.useState<string>('')
+  const [usernameError, setUsernameError] = React.useState<string>('')
   const [emailError, setEmailError] = React.useState<string>('')
+  const [locationError, setLocationError] = React.useState<string>('')
+  const [bioError, setBioError] = React.useState<string>('')
+  const [oldPasswordError, setOldPasswordError] = React.useState<string>('')
+  const [newPasswordError, setNewPasswordError] = React.useState<string>('')
+  const [rePasswordError, setRePasswordError] = React.useState<string>('')
+
   const [updateAccountError, setUpdateAccountError] = React.useState<string>('')
+  const [changePasswordError, setChangePasswordError] = React.useState<string>('')
 
   const [commentHeight, setCommentHeight] = React.useState<number>(10)
 
   const onUpdateAccount = async () => {
-    await updateAccount.mutateAsync({
-      id: profileId,
-      name: name,
-      username: username,
-      email: email,
-      location: location,
-      bio: bio
-    },
-    {
-      onError: (error: any) => {
-        setUpdateAccountError(error.response?.data?.message)
-        clearFormState()
-        clearErrorState()
+    if (name === '' || /^\s*$/.test(name)) {
+      setNameError('Name is required')
+    } else if (username === '' || /^\s*$/.test(username)) {
+      setUsernameError('Username is required')
+    } else if (email === '' || /^\s*$/.test(email)) {
+      setEmailError('Email is required')
+    } else if (location === '' || /^\s*$/.test(location)) {
+      setLocationError('Location is required')
+    } else if (bio === '' || /^\s*$/.test(bio)) {
+      setBioError('Bio is required')
+    } else {
+      clearErrorState()
+      setIsUpdateAccountLoading(true)
+      await updateAccount.mutateAsync({
+        id: profileId,
+        name: name,
+        username: username,
+        email: email,
+        location: location,
+        bio: bio
       },
-      onSuccess: async () => {
-        Toast('Account Updated Successfully')
-        setUpdateAccountError('')
-        clearFormState()
-        clearErrorState()
-        useNavigate('UserScreen', { id: user.id })
-      }
-    })
+      {
+        onError: (error: any) => {
+          setUpdateAccountError(error.response?.data?.message)
+          clearFormState()
+          clearErrorState()
+          setIsUpdateAccountLoading(false)
+        },
+        onSuccess: async () => {
+          Toast('Account Updated Successfully')
+          setUpdateAccountError('')
+          clearFormState()
+          clearErrorState()
+          setIsUpdateAccountLoading(false)
+          useNavigate('UserScreen', { id: user.id })
+        }
+      })
+    }
+  }
+
+  const onChangePassword = async () => {
+    if (oldPassword === '' || /^\s*$/.test(oldPassword)) {
+      setOldPasswordError('Old password is required')
+    } else if (newPassword === '' || /^\s*$/.test(newPassword)) {
+      setNewPasswordError('New password is required')
+    } else if (rePassword === '' || /^\s*$/.test(rePassword)) {
+      setRePasswordError('Re-enter password is required')
+    } else if (newPassword !== rePassword) {
+      setRePasswordError('Password did not match')
+    } else {
+      clearErrorState()
+      setIsChangePasswordLoading(true)
+      await changePassword.mutateAsync({
+        id: profileId,
+        newPassword: newPassword,
+        oldPassword: oldPassword
+      },
+      {
+        onError: (error: any) => {
+          setChangePasswordError(error.response?.data?.message)
+          setIsChangePasswordLoading(false)
+        },
+        onSuccess: async () => {
+          Toast('Password Updated Successfully')
+          setChangePasswordError('')
+          clearFormState()
+          clearErrorState()
+          setIsChangePasswordLoading(false)
+          useNavigate('UserScreen', { id: user.id })
+        }
+      })
+    }
   }
   
   const validateEmail = (text: string) => {
@@ -78,11 +142,22 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
     setEmail(user.email)
     setLocation(user.location)
     setBio(user.bio)
+    setOldPassword('')
+    setNewPassword('')
+    setRePassword('')
   }
 
   const clearErrorState = () => {
+    setNameError('')
+    setUsernameError('')
     setEmailError('')
+    setLocationError('')
+    setBioError('')
+    setOldPasswordError('')
+    setNewPasswordError('')
+    setRePasswordError('')
     setUpdateAccountError('')
+    setChangePasswordError('')
   }
 
   return (
@@ -111,11 +186,14 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isUpdateAccountLoading}
                 value={name}
                 onChangeText={(text: string) => {
                   setName(text)
+                  setNameError('')
                 }}
               />
+              {nameError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ nameError }</Text>}
             </View>
             <View style={tw`flex flex-col my-1 w-full`}>
               <View style={tw`flex flex-row items-center mx-2 mb-1`}>
@@ -123,11 +201,14 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isUpdateAccountLoading}
                 value={username}
                 onChangeText={(text: string) => {
                   setUsername(text)
+                  setUsernameError('')
                 }}
               />
+              {usernameError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ usernameError }</Text>}
             </View>
             <View style={tw`flex flex-col my-1 w-full`}>
               <View style={tw`flex flex-row items-center mx-2 mb-1`}>
@@ -135,6 +216,7 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isUpdateAccountLoading}
                 value={email}
                 onChangeText={(text: string) => {
                   validateEmail(text)
@@ -148,11 +230,14 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isUpdateAccountLoading}
                 value={location}
                 onChangeText={(text: string) => {
                   setLocation(text)
+                  setLocationError('')
                 }}
               />
+              {locationError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ locationError }</Text>}
             </View>
             <View style={tw`flex flex-col my-1 w-full`}>
               <View style={tw`flex flex-row items-center mx-2 mb-1`}>
@@ -161,27 +246,45 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               <View style={tw`flex-row items-start w-full px-3 py-1 rounded-xl border border-neutral-200 bg-white`}>
                 <TextInput
                   style={[tw`w-full text-sm`, { height: commentHeight }, fonts.fontPoppins]}
+                  editable={!isUpdateAccountLoading}
                   multiline={true}
                   numberOfLines={commentHeight}
                   value={bio}
                   onChangeText={(text: string) => {
                     setBio(text)
+                    setBioError('')
                   }}
                   onContentSizeChange={(e) => {
                     setCommentHeight(e.nativeEvent.contentSize.height)
                   }}
                 />
               </View>
+              {bioError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ bioError }</Text>}
             </View>
-            <TouchableOpacity
-              style={tw`flex flex-row items-center justify-center w-full my-1 p-3 text-sm rounded-xl bg-[#f2b900]`}
-              activeOpacity={0.7}
-              onPress={onUpdateAccount}
-            >
-              <Text style={[tw`text-base text-white`, fonts.fontPoppins]}>Save</Text>
-            </TouchableOpacity>
+            {isUpdateAccountLoading
+              ? <View style={tw`flex flex-row items-center justify-center w-full my-1 p-3 text-sm rounded-xl bg-[#f2b900] bg-opacity-50`}>
+                  <Text style={[tw`text-base text-white`, fonts.fontPoppins]}>Updating...</Text>
+                </View>
+              : <TouchableOpacity
+                  style={tw`flex flex-row items-center justify-center w-full my-1 p-3 text-sm rounded-xl bg-[#f2b900]`}
+                  activeOpacity={0.7}
+                  onPress={onUpdateAccount}
+                >
+                  <Text style={[tw`text-base text-white`, fonts.fontPoppins]}>Save</Text>
+                </TouchableOpacity>
+            }
           </View>
           <View style={tw`flex-col w-full my-3`}>
+            {changePasswordError && (
+              <View style={tw`flex flex-row items-center w-full my-3 px-5 py-3 overflow-hidden rounded-xl bg-red-300`}>
+                <MaterialIcon
+                  name="x-circle-fill"
+                  size="medium"
+                  color="#FF7D7D"
+                />
+                <Text style={[tw`-ml-5 w-full text-center text-sm text-[#FFFFFF]`, fonts.fontPoppinsLight]}>{ changePasswordError }</Text>
+              </View>
+            )}
             <Text style={[tw`mx-2 mb-2 text-xl text-neutral-400`, fonts.fontPoppinsBold]}>Change Password</Text>
             <View style={tw`flex flex-col my-1 w-full`}>
               <View style={tw`flex flex-row items-center mx-2 mb-1`}>
@@ -189,8 +292,15 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isChangePasswordLoading}
                 secureTextEntry={true}
+                value={oldPassword}
+                onChangeText={(text: string) => {
+                  setOldPassword(text)
+                  setOldPasswordError('')
+                }}
               />
+              {oldPasswordError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ oldPasswordError }</Text>}
             </View>
             <View style={tw`flex flex-col my-1 w-full`}>
               <View style={tw`flex flex-row items-center mx-2 mb-1`}>
@@ -198,8 +308,15 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isChangePasswordLoading}
                 secureTextEntry={true}
+                value={newPassword}
+                onChangeText={(text: string) => {
+                  setNewPassword(text)
+                  setNewPasswordError('')
+                }}
               />
+              {newPasswordError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ newPasswordError }</Text>}
             </View>
             <View style={tw`flex flex-col my-1 w-full`}>
               <View style={tw`flex flex-row items-center mx-2 mb-1`}>
@@ -207,18 +324,28 @@ const UserSettingsLayout: React.FC<TypedProps> = ({ user, profileId }) => {
               </View>
               <TextInput
                 style={[tw`flex w-full px-3 py-2 text-sm rounded-xl border border-neutral-200 bg-white`, fonts.fontPoppins]}
+                editable={!isChangePasswordLoading}
                 secureTextEntry={true}
+                value={rePassword}
+                onChangeText={(text: string) => {
+                  setRePassword(text)
+                  setRePasswordError('')
+                }}
               />
+              {rePasswordError && <Text style={[tw`text-xs text-red-600 mx-2 my-1`, fonts.fontPoppinsLight]}>{ rePasswordError }</Text>}
             </View>
-            <TouchableOpacity
-              style={tw`flex flex-row items-center justify-center w-full my-1 p-3 text-sm rounded-xl bg-neutral-600`}
-              activeOpacity={0.7}
-              onPress={() => {
-                console.log('Sign In')
-              }}
-            >
-              <Text style={[tw`text-base text-white`, fonts.fontPoppins]}>Update Password</Text>
-            </TouchableOpacity>
+            {isChangePasswordLoading
+              ? <View style={tw`flex flex-row items-center justify-center w-full my-1 p-3 text-sm rounded-xl bg-neutral-600 bg-opacity-50`}>
+                  <Text style={[tw`text-base text-white`, fonts.fontPoppins]}>Updating...</Text>
+                </View>
+              : <TouchableOpacity
+                  style={tw`flex flex-row items-center justify-center w-full my-1 p-3 text-sm rounded-xl bg-neutral-600`}
+                  activeOpacity={0.7}
+                  onPress={onChangePassword}
+                >
+                  <Text style={[tw`text-base text-white`, fonts.fontPoppins]}>Update Password</Text>
+                </TouchableOpacity>
+            }
           </View>
         </View>
       </ScrollView>
