4aea0adae22e8b380f0c190d2534002836aeb0f2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-11-06 16:18:01 +0800

    style: update about and privacy policy layout (#77)

--- src/pages/MainScreen/Misc/AboutScreen.tsx
+++ src/pages/MainScreen/Misc/AboutScreen.tsx
@@ -4,6 +4,7 @@ import MainSplashScreen from '../../../components/SplashScreens/MainSplashScreen
 import ErrorScreen from '../../../components/SplashScreens/ErrorScreen'
 import tw from 'twrnc'
 import { fonts } from '../../../styles/global'
+import { FeatherIcon } from '../../../utils/Icons'
 import { ScrollView, View, Text, Image } from 'react-native'
 import { useGetUser } from '../../../lib/ReactQuery'
 
@@ -16,24 +17,31 @@ const AboutScreen = () => {
 
   return (
     <MainLayout user={user}>
-      <ScrollView contentContainerStyle={tw`flex-1 px-3`}>
-        <View style={tw`flex-col items-start justify-between w-full h-full p-2`}>
-          <View style={tw`flex-col w-full`}>
-            <View>
-              <Text style={[tw`text-2xl text-neutral-600 my-2`, fonts.fontPoppinsBold]}>About App</Text>
-              <Text style={[tw`text-justify text-lg text-neutral-500`, fonts.fontPoppins]}>
+      <ScrollView style={tw`flex-1`}>
+        <View style={tw`flex-col items-center justify-between w-full h-full p-3`}>
+          <View style={tw`flex-col items-center w-full`}>
+            <View style={tw`my-5`}>
+              <FeatherIcon
+                name="code"
+                size="ultraLarge"
+                color="#f2b900"
+              />
+            </View>
+            <View style={tw`flex-col items-center justify-center w-full`}>
+              <Text style={[tw`text-xl text-neutral-600 my-2`, fonts.fontPoppinsBold]}>About App</Text>
+              <Text style={[tw`text-center text-base text-neutral-500`, fonts.fontPoppins]}>
                 Rekados is a social media app for people who want to share and explore new recipes and dishes.
                 With Rekados, you can easily find and follow the cooks and foodies that inspire you, and get recommendations for new recipes and dishes to try.
                 You can also use Rekados to save your favorite recipes.
               </Text>
-              <Text style={[tw`text-justify text-lg text-neutral-500 my-3`, fonts.fontPoppins]}>
+              <Text style={[tw`text-center text-base text-neutral-500 my-3`, fonts.fontPoppins]}>
                 Rekados was founded in 2022 by two food lovers who wanted to create a space where people could connect with others who shared their passion for food.
                 We believe that food is a universal language that brings people together, and we are committed to promoting exploration and discovery through our app.
               </Text>
             </View>
-            <View>
-              <Text style={[tw`text-2xl text-neutral-600`, fonts.fontPoppinsBold]}>Developer</Text>
-              <Text style={[tw`text-justify text-lg text-neutral-500`, fonts.fontPoppins]}>
+            <View style={tw`flex-col items-center justify-center w-full my-2`}>
+              <Text style={[tw`text-xl text-neutral-600`, fonts.fontPoppinsBold]}>Developer</Text>
+              <Text style={[tw`text-center text-base text-neutral-500`, fonts.fontPoppins]}>
                 Rekados was designed and developed by Jerome Villaruel (VEOSCRIPT), 2022.
               </Text>
             </View>
