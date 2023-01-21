3b9b934abfaf8ce805e18a2143b8bbb0fda0b15e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-01-21 15:56:24 +0800

    feat: initialize notifee for push-notification

--- index.js
+++ index.js
@@ -3,23 +3,44 @@
  */
 
 import {AppRegistry} from 'react-native';
-import notifee, { EventType } from '@notifee/react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-notifee.onBackgroundEvent(async ({ type, detail }) => {
-  const { notification, pressAction } = detail;
+import { API_URL } from '@env';
+import io from 'socket.io-client';
+import notifee, { AndroidImportance } from '@notifee/react-native';
 
-  // Check if the user pressed the "Mark as read" action
-  if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
-    // Update external API
-    // await fetch(`https://my-api.com/chat/${notification.data.chatId}/read`, {
-    //   method: 'POST',
-    // });
+const socket = io(API_URL);
 
-    // Remove the notification
-    await notifee.cancelNotification(notification.id);
-  }
+socket.on('connect', () => {
+  console.log('Connected to socket.io server');
+});
+
+socket.on('new_notification', async (data) => {
+  console.log('Received new notification:', data);
+
+  // Request permissions (required for iOS)
+  await notifee.requestPermission()
+
+  // Create a channel (required for Android)
+  const channelId = await notifee.createChannel({
+    id: data.userId === data.userLoggedIn ? 'default' : data.userId,
+    name: 'Rekados Push Notification',
+    importance: data.userId === data.userLoggedIn ? AndroidImportance.NONE : AndroidImportance.HIGH,
+  });
+
+  // Display a notification
+  await notifee.displayNotification({
+    title: data.title,
+    body: data.message,
+    android: {
+      channelId,
+      // pressAction is needed if you want the notification to open the app when pressed
+      pressAction: {
+        id: 'default',
+      },
+    },
+  });
 });
 
 AppRegistry.registerComponent(appName, () => App);
